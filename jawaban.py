dc2ab37254e483b2797a519d1e0a413f2393e17f
Annisa as Atmaraloka <annisa.taryono@gmail.com>
2024-11-23 14:47:09 +0700

    jawaban.py
    berikut ini adalah codingan menggunakan
    bahasa pemrograman python yang menjelaskan tipe data yang ada di python beserta nilai dari tipenya.
    terimakasih.

---
+++ jawaban.py
@@ -0,0 +1,41 @@
+# Tipe Data Integer (bilangan bulat)
+angka = 100
+print("Tipe Data Integer:", angka)
+
+# Tipe Data Float (bilangan desimal)
+desimal = 3.14159
+print("Tipe Data Float:", desimal)
+
+# Tipe Data Complex (bilangan kompleks)
+kompleks = 2 + 3j
+print("Tipe Data Complex:", kompleks)
+
+# Tipe Data String (teks)
+teks = "Annisa Menyukai Bahasa Pemrograman Python!"
+print("Tipe Data String:", teks)
+
+# Tipe Data Boolean (nilai True atau False)
+boolean_true = True
+boolean_false = False
+print("Tipe Data Boolean True:", boolean_true)
+print("Tipe Data Boolean False:", boolean_false)
+
+# Tipe Data List (struktur data seperti array)
+list_angka = [1, 2, 3, 4, 5]
+print("Tipe Data List:", list_angka)
+
+# Tipe Data Tuple (struktur data yang tidak bisa diubah setelah dibuat)
+tuple_angka = (10, 20, 30)
+print("Tipe Data Tuple:", tuple_angka)
+
+# Tipe Data Set (koleksi data unik tanpa urutan)
+set_angka = {10, 20, 30, 40}
+print("Tipe Data Set:", set_angka)
+
+# Tipe Data Dictionary (struktur data pasangan kunci-nilai)
+dict_angka = {'satu': 1, 'dua': 2, 'tiga': 3}
+print("Tipe Data Dictionary:", dict_angka)
+
+# Tipe Data None (tipe data kosong atau tidak ada nilai)
+none_value = None
+print("Tipe Data None:", none_value)
