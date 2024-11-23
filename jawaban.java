23bfc3e75f4de25d6185852cd42ffd32ce93b8f2
Annisa as Atmaraloka <annisa.taryono@gmail.com>
2024-11-23 14:39:37 +0700

    jawaban.java
    berikut ini adalah codingan menggunakan
    bahasa pemrograman java serta tipe data yang ada di java dengan nilai dari tipe datanya.
    terimakasih.

---
+++ jawaban.java
@@ -0,0 +1,47 @@
+public class TipeDataJava {
+    public static void main(String[] args) {
+        // Tipe Data Integer (bilangan bulat)
+        int angka = 100;
+        System.out.println("Tipe Data int: " + angka);
+
+        // Tipe Data Long (bilangan bulat lebih besar)
+        long angkaBesar = 10000000000L;
+        System.out.println("Tipe Data long: " + angkaBesar);
+
+        // Tipe Data Double (bilangan desimal)
+        double desimal = 3.14159;
+        System.out.println("Tipe Data double: " + desimal);
+
+        // Tipe Data Float (bilangan desimal lebih kecil)
+        float desimalFloat = 3.14f;
+        System.out.println("Tipe Data float: " + desimalFloat);
+
+        // Tipe Data Char (karakter tunggal)
+        char karakter = 'A';
+        System.out.println("Tipe Data char: " + karakter);
+
+        // Tipe Data Boolean (nilai benar/salah)
+        boolean isJavaFun = true;
+        System.out.println("Tipe Data boolean: " + isJavaFun);
+
+        // Tipe Data String (teks)
+        String pesan = "Selamat belajar Java!";
+        System.out.println("Tipe Data String: " + pesan);
+
+        // Tipe Data Byte (bilangan bulat kecil)
+        byte angkaKecil = 120;
+        System.out.println("Tipe Data byte: " + angkaKecil);
+
+        // Tipe Data Short (bilangan bulat sedang)
+        short angkaSedang = 32000;
+        System.out.println("Tipe Data short: " + angkaSedang);
+
+        // Tipe Data Integer maksimum
+        Integer angkaMax = Integer.MAX_VALUE;
+        System.out.println("Nilai maksimum Integer: " + angkaMax);
+
+        // Tipe Data Double minimum
+        Double angkaMin = Double.MIN_VALUE;
+        System.out.println("Nilai minimum Double: " + angkaMin);
+    }
+}
